e04bf854116f445dc0e8ec1ba5d1446f9cfd0bec
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2022-04-04 18:48:13 -0300

    Not used...

--- test/tarefa_test.dart
+++ test/tarefa_test.dart
@@ -1,8 +1,8 @@
-import 'package:tarefa/tarefa.dart';
-import 'package:test/test.dart';
+// import 'package:tarefa/tarefa.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
-}
+// void main() {
+//   // test('calculate', () {
+//   //   expect(calculate(), 42);
+//   // });
+// }
